9e8f4ab03c5622d76205f87c04b5dc580219fd64
lmh <742050980@qq.com>
2020-07-06 19:58:32 +0800

    11

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: '个人主页', 
-  description: '姜帅杰的博客',
-  head: [
-      ['link', { rel: 'icon', href: '/img/logo.ico' }],
-      ['link', { rel: 'manifest', href: '/manifest.json' }],
-  ],
-    base : '/springcloud-alibaba-doc/'
-  }
+    title: '个人主页',
+    description: 'lucky博客',
+    head: [
+        ['link', {rel: 'icon', href: '/img/logo.ico'}],
+        ['link', {rel: 'manifest', href: '/manifest.json'}],
+    ],
+    base: '/springcloud-alibaba-doc/'
+}
